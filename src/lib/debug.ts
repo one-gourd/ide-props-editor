db04669f435d7b8875242fabd6817220e47d490c
minghe <minghe36@126.com>
2019-04-29 17:57:29 +0800

    Updates

--- src/lib/debug.ts
+++
@@ -1,7 +0,0 @@
-import { createDebuggers } from 'ide-lib-utils';
-// 默认用名字导出
-const { debugBase, debugComp, debugError, debugIO, debugInteract, debugMini, debugModel, debugRender } = createDebuggers('props-editor');
-
-export {
-    debugBase, debugComp, debugError, debugIO, debugInteract, debugMini, debugModel, debugRender
-};
